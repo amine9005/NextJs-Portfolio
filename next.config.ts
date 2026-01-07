6eaf504df8826ff6775b66655ac792be3593725c
amine9005 <amamselmitrader@gmail.com>
2026-01-07 17:32:20 +0100

    updates

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 import { withNextVideo } from "next-video/process";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL("https://images.unsplash.com/**")],
+  },
 };
 
 export default withNextVideo(nextConfig);
