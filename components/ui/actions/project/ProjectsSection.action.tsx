c39d60595a51152acbea365703db59ba80d3c59d
amine9005 <amamselmitrader@gmail.com>
2026-02-22 14:15:35 +0100

    updates

--- components/ui/actions/project/ProjectsSection.action.tsx
+++ components/ui/actions/project/ProjectsSection.action.tsx
@@ -1,21 +1,46 @@
+"use client";
 import ProjectCard from "@/components/ui/organisms/cards/project/ProjectCard.organism";
 import { BlurFade } from "@/components/ui/Effects/blur-fade";
+import { useProjectsQuery } from "@/hooks/queries/useProjectQuery.hook";
+import { Loader2Icon } from "lucide-react";
+import { P } from "@/components/ui/atoms/text/Text";
+import { ProjectType } from "@/types/project.types";
+import Link from "next/link";
 
 const ProjectsAction = () => {
-  const images = [1, 2, 3];
+  const { data, isLoading, error } = useProjectsQuery();
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col justify-center items-center gap-4">
+          <Loader2Icon className="animate-spin size-10" />
+          <P>Loading...</P>
+        </div>
+      </div>
+    );
+  }
+
+  if (error || !data) {
+    return <></>;
+  }
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-10 p-8 lg:px-24 ">
-      {images.map((_, idx) => (
-        <BlurFade onlyOnce={false} key={idx} delay={0.3 + idx * 0.1} inView>
-          <ProjectCard
-            card={{
-              title: "Project Name",
-              description: "This is a card description.",
-            }}
-          />
-        </BlurFade>
-      ))}
+      {data.map((project: ProjectType, idx: number) =>
+        project.isFeatured ? (
+          <BlurFade onlyOnce={false} key={idx} delay={0.3 + idx * 0.1} inView>
+            <Link href={"project/" + project._id}>
+              <ProjectCard
+                card={{
+                  title: project.title,
+                  description: project.description,
+                }}
+              />
+            </Link>
+          </BlurFade>
+        ) : null,
+      )}
     </div>
   );
 };
